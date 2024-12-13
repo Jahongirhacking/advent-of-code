ad9b2b4a3b915bdda05a9aa3f5a9c9229b864700
Jahongir Hayitov <Jahongirhacking@gmail.com>
2024-12-13 17:45:00 +0500

    feat: add day13 solution

---
+++ 2024/day13/part2/index.ts
@@ -0,0 +1,47 @@
+import * as fs from "fs";
+import * as path from "path";
+
+const solve = (input: string) => {
+  const machines = input.split("\n\n");
+  let tokens = 0;
+  const add = 10000000000000;
+
+  for (const machine of machines) {
+    const lines = machine.split("\n");
+    const [x1, y1] = lines[0]
+      .match(/X\+(\d+), Y\+(\d+)/)!
+      .slice(1)
+      .map(Number);
+    const [x2, y2] = lines[1]
+      .match(/X\+(\d+), Y\+(\d+)/)!
+      .slice(1)
+      .map(Number);
+    const [c, d] = lines[2]
+      .match(/X=(\d+), Y=(\d+)/)!
+      .slice(1)
+      .map((num) => Number(num) + add);
+
+    // Cramer's rule
+    const denominator = x1 * y2 - y1 * x2;
+    if (denominator === 0) continue;
+
+    const a = (c * y2 - d * x2) / denominator;
+    const b = (d * x1 - c * y1) / denominator;
+
+    if (Number.isInteger(a) && Number.isInteger(b)) {
+      tokens += 3 * a + b;
+    }
+  }
+
+  return tokens;
+};
+
+// Read the whole input file synchronously
+const input = fs.readFileSync(
+  path.join(path.resolve(__dirname, ".."), "input.txt"),
+  "utf8"
+);
+
+// Pass the input to the solve function
+const answer = solve(input);
+console.log(answer);
